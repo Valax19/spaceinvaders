ff07d9e66eb6ca879941e9cb4b843b321d3da6e0
r3m19 <r3m194@gmail.com>
2021-05-11 15:04:31 +0200

    Déplacer le vaisseau vers la gauche

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -81,6 +81,11 @@ public class SpaceInvaders {
 		 public void deplacerVaisseauVersLaDroite() {
 		        if (vaisseau.abscisse()< (longueur-1)) vaisseau.seDeplacerVersLaDroite();
 			}
+
+
+		public void deplacerVaisseauVersLaGauche() {
+			if(vaisseau.abscisse() > (0)) vaisseau.seDeplacerVersLaGauche();
+		}
 		 
 		
  }
