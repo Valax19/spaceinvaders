e08f34f86320bcb72d4c8f88c94dc7a95f729693
r3m19 <r3m194@gmail.com>
2021-05-19 15:46:12 +0200

    Rajout des images de la semaine 3

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -13,20 +13,20 @@ public class Vaisseau {
 		this.y = y;
 	}
 
-	public Vaisseau(int longueur, int hauteur, int x2, int y2) {
+	public Vaisseau(int longueur, int hauteur, int x, int y) {
 		this.longueur=longueur;
 		   this.hauteur=hauteur;
 		   this.x = x;
 		   this.y = y;
 	}
 
-	 public boolean occupeLaPosition(int x, int y) {
-	     if ((this.x<=x) && (x<=this.x+this.longueur-1)) 
-		      if ( (this.y-this.hauteur+1<=y) && (y<=this.y))
-			  return true;
-		
-	     return false;
-     }
+	   public boolean occupeLaPosition(int x, int y) {
+		     if ((this.x<=x) && (x<=this.x+this.longueur-1)) 
+			      if ( (this.y-this.hauteur+1<=y) && (y<=this.y))
+				  return true;
+			
+		     return false;
+	     }
 	
 	public void seDeplacerVersLaDroite() {
 	      this.x = this.x + 1 ;
